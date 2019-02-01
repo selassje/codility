db0a9bfcd02e0feb68a5eb397c2295b9c64ca58c
Przemyslaw Koziol <przemkoz85@gmail.com>
2019-02-01 02:43:44 +0100

    ffasaadasda

--- rubidium2018/build/gnu/build.sh
+++ rubidium2018/build/gnu/build.sh
@@ -2,6 +2,4 @@ export CC=/usr/bin/gcc-6
 export CXX=/usr/bin/g++-6
 conan install -s arch=x86_64 -s arch_build=x86_64 ../..
 cmake "../.."
-cat Makefile
-make 2>&1
-#cmake --build .
+cmake --build .
