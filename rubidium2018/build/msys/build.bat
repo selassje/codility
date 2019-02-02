df5d8a88b966dbb49ac290e6386d5e63fed6da0e
Przemyslaw Koziol <przemkoz85@gmail.com>
2019-02-02 18:09:58 +0100

    updated mingw and msys build scripts

--- rubidium2018/build/msys/build.bat
+++ rubidium2018/build/msys/build.bat
@@ -1,3 +1,5 @@
+export CXX=mingw32-g++.exe
+export CC=mingw32-gcc.exe
 conan  install -s arch=x86 -s arch_build=x86 -s compiler=gcc -s compiler.libcxx=libstdc++11 -s compiler.version=6.3 --build=outdated ../..
 cmake ../.. -G "MSYS Makefiles"
 cmake --build . --config Release
