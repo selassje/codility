87e47ff82eb95ad9528eef56fe8f9aece7141838
Przemyslaw Koziol <przemkoz85@gmail.com>
2019-02-03 23:06:26 +0100

    coverity scirpts updated

--- rubidium2018/build/mingw/coverityScan.bat
+++ rubidium2018/build/mingw/coverityScan.bat
@@ -1,1 +1,5 @@
-cov-build --dir cov-int build.bat
+set CXX=mingw32-g++.exe
+set CC=mingw32-gcc.exe
+conan  install -s arch=x86 -s arch_build=x86 -s compiler=gcc -s compiler.libcxx=libstdc++11 -s compiler.version=6.3 --build=outdated ../..
+cmake  ../.. -G "MinGW Makefiles" 
+cov-build --dir cov-int "mingw32-make.exe"
--- rubidium2018/build/msvc/coverityScan.bat
+++ rubidium2018/build/msvc/coverityScan.bat
@@ -1,2 +1,3 @@
+conan  install -s arch=x86 -s arch_build=x86 -build=outdated ../..
 cmake ../..
 cov-build --dir cov-int cmake --build . --config Release
