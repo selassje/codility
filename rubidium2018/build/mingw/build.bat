6a436f182249f2bb25a98f12ebbe0fdd606552cd
Przemyslaw Koziol <przemkoz85@gmail.com>
2019-02-04 00:01:43 +0100

    asdasdasd

--- rubidium2018/build/mingw/build.bat
+++ rubidium2018/build/mingw/build.bat
@@ -2,5 +2,5 @@ set CXX=mingw32-g++.exe
 set CC=mingw32-gcc.exe
 conan  install -s arch=x86 -s arch_build=x86 -s compiler=gcc -s compiler.libcxx=libstdc++11 -s compiler.version=6.3 --build=outdated ../..
 cmake  ../.. -G "MinGW Makefiles" 
-cmake --build . --config Release
+cmake --build .
 
--- rubidium2018/build/mingw/coverityScan.bat
+++ rubidium2018/build/mingw/coverityScan.bat
@@ -2,4 +2,5 @@ set CXX=mingw32-g++.exe
 set CC=mingw32-gcc.exe
 conan  install -s arch=x86 -s arch_build=x86 -s compiler=gcc -s compiler.libcxx=libstdc++11 -s compiler.version=6.3 --build=outdated ../..
 cmake  ../.. -G "MinGW Makefiles" 
-cov-build --dir cov-int "mingw32-make.exe"
+cov-build --dir cov-int cmake --build .
+
